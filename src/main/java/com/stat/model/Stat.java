8635a4f8743ca02140dcd21012529e5942fce668
Manish <manishmbm2010@gmail.com>
2018-07-22 17:48:47 +0200

    Chnages for Supporting concurrent reads

--- src/main/java/com/stat/model/Stat.java
+++ src/main/java/com/stat/model/Stat.java
@@ -1,9 +1,17 @@
 package com.stat.model;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 /*Model class in order to maintain the statistics of transaction initiated in last 60 seconds*/
+
 public class Stat {
 
 	private static Stat stats;
+	private static final ReadWriteLock readWriteLock = new ReentrantReadWriteLock(true);
+	private static final Lock readLock = readWriteLock.readLock();
+	private static final Lock writeLock = readWriteLock.writeLock();
 	double sum;
 	double avg;
 	double max;
@@ -16,43 +24,50 @@ public class Stat {
 
 	// Create the task instance if it is not created already
 	public static Stat getInstance() {
-		if (stats == null) {
-			stats = new Stat();
+		readLock.lock();
+		try {
+			if (stats == null) {
+				stats = new Stat();
+			}
+		} finally {
+			readLock.unlock();
 		}
 		return stats;
 	}
 
-	public synchronized void setStats(double sum, double avg, double max, double min, long count) {
-		this.sum = sum;
-		this.avg = avg;
-		/*
-		 * while (true) {
-		 * 
-		 * Thread.sleep(60000); break; }
-		 */
-		this.max = max;
-		this.min = min;
-		this.count = count;
+	public void setStats(double sum, double avg, double max, double min, long count) {
+		writeLock.lock();
+		try {
+			this.sum = sum;
+			this.avg = avg;
+			// while (true) { Thread.sleep(6000); break; }
+			this.max = max;
+			this.min = min;
+			this.count = count;
+		} finally {
+			writeLock.unlock();
+
+		}
 
 	}
 
-	public synchronized double getSum() {
+	public double getSum() {
 		return sum;
 	}
 
-	public synchronized double getAvg() {
+	public double getAvg() {
 		return avg;
 	}
 
-	public synchronized double getMax() {
+	public double getMax() {
 		return max;
 	}
 
-	public synchronized double getMin() {
+	public double getMin() {
 		return min;
 	}
 
-	public synchronized long getCount() {
+	public long getCount() {
 		return count;
 	}
 
